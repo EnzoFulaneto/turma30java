be9500891d646b0f38bdd279f03fac90e5364a70
fulaneto <fulanetoenzo@gmail.com>
2021-08-03 23:27:30 -0300

    Trabalhos

--- java/AULAS/src/lista2_LaçoDeCondição/Lista2JavaEx2.java
+++ java/AULAS/src/lista2_LaçoDeCondição/Lista2JavaEx2.java
@@ -1,10 +1,24 @@
 package lista2_LaçoDeCondição;
 
+import java.util.Scanner;
+
+//Faça um programa que entre com três números e coloque em ordem crescente.
 public class Lista2JavaEx2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner leia = new Scanner (System.in);
+		int n1, n2, n3;
+		
+		System.out.println("Insira o primeiro valor:");
+		n1 = leia.nextInt();
+		System.out.println("Insira o segundo valor:");
+		n2 = leia.nextInt();
+		System.out.println("Insira o terceiro valor:");
+		n3 = leia.nextInt();
+		
+		if (n1 > n2 && n2 > n3) {
+			System.out.println(n1+"\n"+n2+"\n"+n3);
+		}
 	}
 
 }
