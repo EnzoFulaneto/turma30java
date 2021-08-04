9044b83b85472af3f1bf6a204dd0b3ff94176521
fulaneto <fulanetoenzo@gmail.com>
2021-08-04 17:49:26 -0300

    Listas Java

--- java/AULAS/src/lista2_LaçoDeCondição/Lista2JavaEx2.java
+++ java/AULAS/src/lista2_LaçoDeCondição/Lista2JavaEx2.java
@@ -8,7 +8,38 @@ public class Lista2JavaEx2 {
 	Scanner leia = new Scanner (System.in);
 	int n1, n2, n3;
 	
-	System.out.println("");
+	
+	System.out.println("Insira o primeiro valor: ");
+	n1 = leia.nextInt();
+	System.out.println("Insira o segundo valor: ");
+	n2 = leia.nextInt();
+	System.out.println("Insira o terceiro valor: ");
+	n3 = leia.nextInt();
+	
+	if (n1 > n2 && n1 > n3) {
+		if(n2 > n3) {
+			System.out.println(n3+"\n"+n2+"\n"+n1);
+		}
+		else {
+			System.out.println(n2+"\n"+n3+"\n"+n1);
+		}}
+	else if (n2 > n1 && n2 > n3) {
+		if (n1 > n3) {
+			System.out.println(n3+"\n"+n1+"\n"+n2);
+		}
+		else {
+			System.out.println(n1+"\n"+n3+"\n"+n2);
+		}}
+	else {
+		if (n1 > n2) {
+			System.out.println(n2+"\n"+n1+"\n"+n3);
+		}
+		else {
+			System.out.println(n1+"\n"+n2+"\n"+n3);
+		}
 	}
+	}
+	
+	}
+	
 
-}
