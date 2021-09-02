7e0cea265b09a8f04d0b1af70e78583acc46fc5f
fulaneto <fulanetoenzo@gmail.com>
2021-09-02 01:33:02 -0300

    Listas Spring

--- Spring/MinhaLojaDeGames/src/main/java/com/MLDG/MinhaLojaDeGames/repository/CategoriaRepository.java
+++ Spring/MinhaLojaDeGames/src/main/java/com/MLDG/MinhaLojaDeGames/repository/CategoriaRepository.java
@@ -1,11 +1,14 @@
 package com.MLDG.MinhaLojaDeGames.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.MLDG.MinhaLojaDeGames.model.CategoriaModel;
+import com.MLDG.MinhaLojaDeGames.model.Categoria;
 
-public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long>{
-
-	Object findAllByCategoriaContainingIgnoreCase(String categoria);
-
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	
+	public List <Categoria> findAllByCategoriaContainingIgnoreCase (String categoria);
 }
